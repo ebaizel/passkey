bd98f0f417210e8280fe079f0507fb717a9381e1
emile baizel <ebaizel@gmail.com>
2024-04-10 06:02:13 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// JavaScript code to execute
+function executeJavaScript() {
+    alert('JavaScript executed!');
+}
+
+// Add event listener to button
+document.getElementById('executeButton').addEventListener('click', executeJavaScript);
